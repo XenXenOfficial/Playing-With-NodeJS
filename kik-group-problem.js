5073c4e7a9b88c91c028c5060396d56bf7583894
XenXenOfficial <9jaydon99@gmail.com>
2019-06-29 02:35:51 -0400

    Update kik-group-problem.js

--- kik-group-problem.js
+++ kik-group-problem.js
@@ -1,17 +1,19 @@
-let primes = [];
-let n1 = 17;
-let n2 = 80;
-
 function checkPrime(prime){
     let start = 2;
+    //A quick check if the num is a prime by starting at the square root of the number
+    //Its now O(sqrt(n))
     while(start <= Math.sqrt(prime)){if(prime%start++<1) return false;}
     return prime > 1
 }
 
 function createPrimes(max, min=2){
+    //Constraints listed in the file
     if(min<=2) return false;
     if(100<=max) return false;
     if(max-min<=35) return false;
+    //[...Array(max).keys()] is just a fancy way of creating an array of integers
+    //Filter just a nifty function that'll remove items that don't pass a function
+    //In this case, it's just checking if its a prime.
     return [...Array(max).keys()].splice(min, max).filter(_=>{
         return checkPrime(_)
     });
@@ -19,9 +21,10 @@ function createPrimes(max, min=2){
 
 function createCombinations(primes){
     let combinations = [];
+    //This just creates the combinations of numbers
     primes.map(_=>{
         for(var i = 0; i < primes.length; i++){
-            if(!(_==primes[i])){
+            if(!(_==primes[i])){ //This check makes sure its unique and doesn't include repeats with itself. 
                 combinations.push(`${_}${primes[i]}`)
             }
         }
@@ -30,6 +33,9 @@ function createCombinations(primes){
 }
 
 function fibonacci(min, max, length){
+    //This generates the fibonacci sequence from the min and max value
+    //Tbh, stolen from: 
+    //https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
     var a=parseInt(max), b=min, temp;
     while(length >= 0){
         temp = a;
@@ -43,8 +49,11 @@ function fibonacci(min, max, length){
 function start(n2, n1){
     let primes = createPrimes(n2, n1);
     let combinations = createCombinations(primes);
+    //Gets new primes from the combinations.
     let newPrimes = combinations.filter(_=>checkPrime(_));
+    //The numbers are already sorted, so the min value is the first value.
     let smallest = primes[0];
+    //Same goes for largest, but the last value.
     let largest = newPrimes[newPrimes.length-1];
     let lastNumFib = fibonacci(smallest, largest, newPrimes.length)
     console.log(`1st Prime List: ${primes}\n\nCombination of all primes: ${combinations}\n\n2nd prime list: ${newPrimes}\n\n
